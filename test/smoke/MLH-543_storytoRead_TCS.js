5a30923b6642034e8023aacb75237ae5c95af98b
stu <tregubovde@gmail.com>
2020-11-12 14:40:44 -0800

    corrected style - deleted empty spaces

--- test/smoke/MLH-543_storytoRead_TCS.js
+++ test/smoke/MLH-543_storytoRead_TCS.js
@@ -6,25 +6,25 @@ const inputValues4AndClick = require('../../helpers/inputValues4AndClick');
 
 describe('MLH-543 Page header is present and correct  ', function() {
 
-    xit('TCS-001 page header My little hero is present', function (){
+    it('TCS-001 page header My little hero is present', function (){
         browser.url('');
         const headerMLH = $(sel.headerMLH).isDisplayed();
         expect(headerMLH).toEqual(true);
     });
 
-    xit('TCS-002 page header My little hero is correct', function (){
+    it('TCS-002 page header My little hero is correct', function (){
         const headerMLH = $(sel.headerMLH).getText();
         expect(headerMLH).toEqual(exp.headerMLH);
     });
 //TCS-003 unresolved yet
-    xit('TCS-004 uploaded image has size <=500px', function (){
+    it('TCS-004 uploaded image has size <=500px', function (){
         inputValues5AndClick(data.name,data.gender.she,data.age,data.storyType.Comedy);
         const uploadedPictureSize = $(sel.uploadedPicture).getAttribute('width');
         const toNumber = parseInt(uploadedPictureSize, 10);
         expect(toNumber).toBeLessThanOrEqual(500);
     });
 
-    xit('TCS-005 have default image on the story', function (){
+    it('TCS-005 have default image on the story', function (){
         browser.url('');
         inputValues4AndClick(data.name,data.gender.she,data.age,data.storyType.Comedy);
         const uploadedPicture = $(sel.uploadedPicture).getAttribute('src');
@@ -37,7 +37,5 @@ describe('MLH-543 Page header is present and correct  ', function() {
         const headerStory = $(sel.headerStory).getText();
         expect(headerStory).toStrictEqual(data.headerStory);
     });
-
-
 
 })
