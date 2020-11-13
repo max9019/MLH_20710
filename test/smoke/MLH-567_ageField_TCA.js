513cb4b5cc62905c7eb3b320d345b94ffec98984
GeneralMunh <mrlednev@gmail.com>
2020-11-13 13:50:01 -0800

    MLH-567 Task added (age). TCA-001, TCA-002, TCA-003.

---
+++ test/smoke/MLH-567_ageField_TCA.js
@@ -0,0 +1,23 @@
+const sel = require('../../data/selectors.json');
+const exp = require('../../data/expected.json');
+
+describe('MLH-567 field age tests', function () {
+
+    it('TCA-001 Verify that a label field is present.', function () {
+        browser.url('');
+        const label = $$(sel.label)[2].isDisplayed();
+        expect(label).toEqual(true);
+    });
+
+    it('TCA-002 Label for age = 3. How old is your hero?', function () {
+        browser.url('');
+        const text = $$(sel.label)[2].getAttribute('title');
+        expect(text).toEqual(exp.labelAge);
+    });
+
+    it('TCA-003 Placeholder Age is displayed', function () {
+        browser.url('');
+        const text = $(sel.age).getAttribute('placeholder');
+        expect(text).toEqual(exp.placeholderAge);
+    });
+});
