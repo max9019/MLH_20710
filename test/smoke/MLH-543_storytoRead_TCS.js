835e0ba75ad3be8aae620555454a8646f06ee279
stu <tregubovde@gmail.com>
2020-11-12 21:39:02 -0800

    fixed

--- test/smoke/MLH-543_storytoRead_TCS.js
+++ test/smoke/MLH-543_storytoRead_TCS.js
@@ -7,32 +7,32 @@ const storyAgeText = require('../../helpers/storyAgeText');
 
 describe('MLH-543 Page header is present and correct  ', function() {
 
-    xit('TCS-001 page header My little hero is present', function (){
+    it('TCS-001 page header My little hero is present', function (){
         browser.url('');
         const headerMLH = $(sel.headerMLH).isDisplayed();
         expect(headerMLH).toEqual(true);
     });
 
-    xit('TCS-002 page header My little hero is correct', function (){
+    it('TCS-002 page header My little hero is correct', function (){
         const headerMLH = $(sel.headerMLH).getText();
         expect(headerMLH).toEqual(exp.headerMLH);
     });
 //TCS-003 unresolved yet
-    xit('TCS-004 uploaded image has size <=500px', function (){
+    it('TCS-004 uploaded image has size <=500px', function (){
         inputValues5AndClick(data.name,data.gender.she,data.age,data.storyType.Comedy);
         const uploadedPictureSize = $(sel.uploadedPicture).getAttribute('width');
         const toNumber = parseInt(uploadedPictureSize, 10);
         expect(toNumber).toBeLessThanOrEqual(500);
     });
 
-    xit('TCS-005 have default image on the story', function (){
+    it('TCS-005 have default image on the story', function (){
         browser.url('');
         inputValues4AndClick(data.name,data.gender.she,data.age,data.storyType.Comedy);
         const uploadedPicture = $(sel.uploadedPicture).getAttribute('src');
         expect(uploadedPicture).toContain('app/static');
     });
 
-    xit('TCS-006 the header of the story is in capital letters', function (){
+    it('TCS-006 the header of the story is in capital letters', function (){
         browser.url('');
         inputValues4AndClick(data.name,data.gender.she,data.age,data.storyType.Comedy);
         const headerStory = $(sel.headerStory).getText();
