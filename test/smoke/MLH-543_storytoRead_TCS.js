480e756d360ef0c2d45ddc3b5e02f46c6d35d61d
stu <tregubovde@gmail.com>
2020-11-15 13:40:58 -0800

    added Try again! to expected, wrote TCS-009

--- test/smoke/MLH-543_storytoRead_TCS.js
+++ test/smoke/MLH-543_storytoRead_TCS.js
@@ -7,49 +7,66 @@ const storyAgeText = require('../../helpers/storyAgeText');
 
 describe('MLH-543 Final Story Testing  ', function() {
 
-    it('TCS-001 page header My little hero is present', function (){
+    xit('TCS-001 page header My little hero is present', function (){
         browser.url('');
         const headerMLH = $(sel.headerMLH).isDisplayed();
         expect(headerMLH).toEqual(true);
     });
 
-    it('TCS-002 page header My little hero is correct', function (){
+    xit('TCS-002 page header My little hero is correct', function (){
         const headerMLH = $(sel.headerMLH).getText();
         expect(headerMLH).toEqual(exp.headerMLH);
     });
 //TCS-003 unresolved yett
-    it('TCS-004 uploaded image has size <=500px', function (){
+    xit('TCS-004 uploaded image has size <=500px', function (){
         inputValues5AndClick(data.name,data.gender.she,data.age,data.storyType.Comedy);
         const uploadedPictureSize = $(sel.uploadedPicture).getAttribute('width');
         const toNumber = parseInt(uploadedPictureSize, 10);
         expect(toNumber).toBeLessThanOrEqual(500);
     });
 
-    it('TCS-005 have default image on the story', function (){
+    xit('TCS-005 have default image on the story', function (){
         browser.url('');
         inputValues4AndClick(data.name,data.gender.she,data.age,data.storyType.Comedy);
         const uploadedPicture = $(sel.uploadedPicture).getAttribute('src');
         expect(uploadedPicture).toContain('app/static');
     });
 
-    it('TCS-006 the header of the story is in capital letters', function (){
+    xit('TCS-006 the header of the story is in capital letters', function (){
         browser.url('');
         inputValues4AndClick(data.name,data.gender.she,data.age,data.storyType.Comedy);
         const headerStory = $(sel.headerStory).getText();
         expect(headerStory).toStrictEqual(exp.headerStory);
     });
 
-    it('TCS-007 the story has age written in words', function (){
+    xit('TCS-007 the story has age written in words', function (){
         browser.url('');
         inputValues4AndClick(data.name,data.gender.she,data.age,data.storyType.Comedy);
         expect(storyAgeText()).toEqual(exp.storyAge);
     });
 
-    it('TCS-008 the moral is present in comedy', function (){
+    xit('TCS-008 the moral is present in comedy', function (){
         browser.url('');
         inputValues4AndClick(data.name,data.gender.she,data.age,data.storyType.Comedy);
         const storyText = $$(sel.storyText)[1].getAttribute('textContent');
         expect(storyText).toStrictEqual(exp.moralComedyText);
     });
+
+    it('TCS-009 try again btn is displayed', function (){
+        browser.url('');
+        inputValues4AndClick(data.name,data.gender.she,data.age,data.storyType.Comedy);
+        const tryAgainBtn = $(sel.tryAgain).waitForDisplayed();
+        expect(tryAgainBtn).toEqual(true);
+    });
+
+    it('TCS-010 try again has the title Try again!', function (){
+        browser.url('');
+        inputValues4AndClick(data.name,data.gender.she,data.age,data.storyType.Comedy);
+        const tryAgainBtn = $(sel.tryAgain).waitForDisplayed();
+        const tryAgainText = $(sel.tryAgain).getAttribute('textContent')
+        expect(tryAgainText).toEqual(true);
+    });
+
+
 
 })
