3a73d386d10479a4c1d79e2d42bdb5bf35c88c43
Lidia <mslednev@gmail.com>
2020-11-17 19:55:55 -0800

    MLH-584 field age tests to check spinners (TCA-004, TCA-005, TCA-018, TCA-019, TCA-020)

--- test/smoke/MLH-584_ageFieldSpinner_TCA.js
+++ test/smoke/MLH-584_ageFieldSpinner_TCA.js
@@ -11,14 +11,39 @@ describe('MLH-584 field age tests to check spinners', function () {
 
     it('TCA-005 Verify that spinner Up is present.', function () {
         browser.url('');
+        $(sel.age).moveTo();
         const spinnerUp = $$(sel.spinner)[0].isDisplayed();
         expect(spinnerUp).toEqual(true);
     });
 
     it('TCA-005 Verify that spinner Down is present.', function () {
         browser.url('');
+        $$(sel.spinner)[1].moveTo();
         const spinnerDown = $$(sel.spinner)[1].isDisplayed();
         expect(spinnerDown).toEqual(true);
     });
 
+    it('TCA-018 Verify that number is change if one time click spinner Up button.', function () {
+        browser.url('');
+        $(sel.age).setValue(1);
+        $$(sel.spinner)[0].click();
+        const Num1 = $(sel.age).getValue();
+        expect(Num1).toEqual("2");
+    });
+
+    it('TCA-019 Verify that number is change if one time click spinner Down button.', function () {
+        browser.url('');
+        $(sel.age).setValue(2);
+        $$(sel.spinner)[1].click();
+        const Num1 = $(sel.age).getValue();
+        expect(Num1).toEqual("1");
+    });
+
+    it('TCA-019 Verify that number 1 is no change if one time click spinner Down button.', function () {
+        browser.url('');
+        $(sel.age).setValue(1);
+        $$(sel.spinner)[1].click();
+        const Num1 = $(sel.age).getValue();
+        expect(Num1).toEqual("1");
+    });
 });
