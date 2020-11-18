a2e5c8c6e22fdd3c09d748b12269d59fe5f165a0
Annieboico <Anastasiia.boico@gmail.com>
2020-11-17 22:48:22 -0800

    MLH-522_StoryFieldFunctionality_TCT.js Update

--- test/regression/MLH-522_StoryFieldFunctionality_TCT.js
+++ test/regression/MLH-522_StoryFieldFunctionality_TCT.js
@@ -9,25 +9,28 @@ describe ('Story Field functionality', function() {
         it ('TCT-004 Story dropdown can choose any story', function () {
             browser.url('https://qa-apps.netlify.app/app_my_hero');
             $(sel.storyClick).click();
-            const storyTypes = $$(sel.storyType)[0].isDisplayed();
+            const storyTypes = $$(sel.storyType)[1].isDisplayed();
             expect(storyTypes).toEqual(true);
         });
         it ('TCT-005 Story dropdown can choose only one type of story', function () {
             browser.url('https://qa-apps.netlify.app/app_my_hero');
             $(sel.storyClick).click();
-            $$(sel.storyType)[5].click();
+            $$(sel.storyType)[0].click();
             $(sel.storyClick).click();
             $$(sel.storyType)[6].click();
             const showStory = $('.ant-select-selection-item').getAttribute('title');
             expect(showStory).toEqual(exp.typeOfStory);
         });
 
-        it ('TCT-006 Story contents', function () {
-            browser.url('https://qa-apps.netlify.app/app_my_hero');
+        it('TCT-006 Story contents', function () {
             $(sel.storyClick).click();
-            const storyTypes = $$(sel.storyType)[0,1,2,3,4,5,6].isDisplayed();
-            expect(storyTypes).toEqual(true);
+            browser.pause(1000);
+            const storyName = $$(sel.storyType)[0,1,2,3,4,5,6].isDisplayed();
+            expect(storyName).toEqual(true);
         });
+
+
+
 
 
 
