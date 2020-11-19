a2c5b4438f2af3f5023929d2a86047ae2023546e
Sergey Makhalov <63439804+Sergy-Mak@users.noreply.github.com>
2020-11-18 21:07:09 -0800

    Update MLH-615_nameField_TCN.js

--- test/regression/MLH-615_nameField_TCN.js
+++ test/regression/MLH-615_nameField_TCN.js
@@ -3,7 +3,7 @@ const symbols = require('../../data/testData.json').symbols;
 
 describe('MLH-615 Testing Name Field - Regression', function () {
 
-    it('TCN-005 Verify that a Input field not accepted more them 70 simbols ', function () {
+    it('TCN-005 Verify what a Input field not accepted more them 70 simbols ', function () {
         browser.url('');
         let nField = $(sel.name).setValue(symbols.moreThanNecessary);
         nField = $(sel.name).getValue();
@@ -11,7 +11,7 @@ describe('MLH-615 Testing Name Field - Regression', function () {
         expect(count).toEqual(70);
     });
 
-    it('TCN-005-1 Verify that a Input field not accepted more them 70 simbols & alert', function () {
+    it('TCN-005-1 Verify what a Input field not accepted more them 70 simbols & alert', function () {
         browser.url('');
         let nField = $(sel.name).setValue(symbols.moreThanNecessary);
         $(sel.alert).waitForDisplayed();
@@ -19,7 +19,7 @@ describe('MLH-615 Testing Name Field - Regression', function () {
         expect(alert).toEqual(true);
     });
 
-    it('TCN-006 Verify that a Input field accepted 70 simbols', function () {
+    it('TCN-006 Verify what a Input field accepted 70 simbols', function () {
         browser.refresh();
         let nField = $(sel.name).setValue(symbols.allowed);
         nField = $(sel.name).getValue();
@@ -27,7 +27,7 @@ describe('MLH-615 Testing Name Field - Regression', function () {
         expect(count1).toEqual(70) ;
     });
 
-    it('TCN-006-1 Verify that a Input field accepted 70 simbols & no alert', function () {
+    it('TCN-006-1 Verify what a Input field accepted 70 simbols & no alert', function () {
         browser.refresh();
         let nField = $(sel.name).setValue(symbols.allowed);
         nField = $(sel.name).getValue();
@@ -35,49 +35,49 @@ describe('MLH-615 Testing Name Field - Regression', function () {
         expect(alert).toEqual(false);
     });
 
-    it('TCN-007 Verify that a Input field accepted single uppercase letters', function () {
+    it('TCN-007 Verify what a Input field accepted single uppercase letters', function () {
         browser.refresh();
         let nField = $(sel.name).setValue(symbols.singleUppercase);
         nField = $(sel.name).getValue();
         expect(nField).toEqual(symbols.singleUppercase);
     });
 
-    it('TCN-008 Verify wthat a Input field accepted single lowercase letters', function () {
+    it('TCN-008 Verify what a Input field accepted single lowercase letters', function () {
         browser.refresh();
         let nField = $(sel.name).setValue(symbols.singleLowercase);
         nField = $(sel.name).getValue();
         expect(nField).toEqual(symbols.singleLowercase);
     });
 
-    it('TCN-009 Verify that a Input field accepted single digit', function () {
+    it('TCN-009 Verify what a Input field accepted single digit', function () {
         browser.refresh();
         let nField = $(sel.name).setValue(symbols.singleDigit);
         nField = $(sel.name).getValue();
         expect(nField).toEqual(symbols.singleDigit);
     });
 
-    it('TCN-010 Verify that a Input field accepted A-Z', function () {
+    it('TCN-010 Verify what a Input field accepted A-Z', function () {
         browser.refresh();
         let nField = $(sel.name).setValue(symbols.uppercase);
         nField = $(sel.name).getValue();
         expect(nField).toEqual(symbols.uppercase);
     });
 
-    it('TCN-011 Verify that a Input field accepted a-z', function () {
+    it('TCN-011 Verify what a Input field accepted a-z', function () {
         browser.refresh();
         let nField = $(sel.name).setValue(symbols.lowercase);
         nField = $(sel.name).getValue();
         expect(nField).toEqual(symbols.lowercase);
     });
 
-    it('TCN-012 Verify tthat a Input field accepted special characters', function () {
+    it('TCN-012 Verify what a Input field accepted special characters', function () {
         browser.refresh();
         let nField = $(sel.name).setValue(symbols.specialCharacters);
         nField = $(sel.name).getValue();
         expect(nField).toEqual(symbols.specialCharacters);
     });
 
-    it('TCN-013 Verify that a Input field accepted digit 0-9', function () {
+    it('TCN-013 Verify what a Input field accepted digit 0-9', function () {
         browser.refresh();
         let nField = $(sel.name).setValue(symbols.digit);
         nField = $(sel.name).getValue();
